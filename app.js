819b2b84c1bf0e1aae137d9771a7bdcfe540edc6
Chengze Fan <fancz2002@gmail.com>
2024-09-08 18:30:54 -0700

    bootstrap, but no fromat

--- app.js
+++ app.js
@@ -1,13 +1,56 @@
 const express = require('express');
 const path = require('path');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
 const app = express();
-const port = 80;
+const port = 3000;
 
+// Set up EJS
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Serve static files
 app.use(express.static('public'));
 
+// Routes
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.render('layout', {
+        title: 'Home',
+        content: res.render('index', { title: 'Home' })
+    });
 });
+
+app.get('/about', (req, res) => {
+    res.render('layout', {
+        title: 'About',
+        content: res.render('about', { title: 'About' })
+    });
+});
+
+app.get('/articles', (req, res) => {
+    const articles = [
+        { title: 'My First Article', content: 'This is the content of my first article.' },
+        { title: 'Another Great Post', content: 'Here\'s another interesting article.' }
+    ];
+    res.render('layout', {
+        title: 'Articles',
+        content: res.render('articles', { title: 'Articles', articles: articles })
+    });
+});
+
+// Middleware to log visitor information
+app.use((req, res, next) => {
+    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    console.log(`Visitor from: ${ip}`);
+    next();
+});
+
+app.use(cookieParser());
+app.use(session({
+    secret: 'your-secret-key',
+    resave: false,
+    saveUninitialized: true
+}));
 
 app.listen(port, '0.0.0.0', () => {
     console.log(`Server running at http://0.0.0.0:${port}`);
